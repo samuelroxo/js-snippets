32bd7db7ef7b9239ad910c56221689fb42057408
Samuel Alves Ferreira <samuel.roxo@gmail.com>
2018-11-13 12:02:15 -0200

    Update Preencher Cadastro.js

--- Preencher Cadastro.js
+++ Preencher Cadastro.js
@@ -1,6 +1,7 @@
 $(function() {
-    var nomes = ['Aaron', 'Adriano', 'Alan', 'Alexandre', 'Alonso', 'Anderson', 'Andres', 'Antônio', 'Benjamin', 'Bruno', 'Camilo', 'Carlos', 'Christian', 'Christopher', 'Cristóvão', 'Daniel', 'Dante', 'David', 'Diego', 'Eduardo', 'Elias', 'Emanuel', 'Emiliano', 'Emílio', 'Estêvão', 'Evandro', 'Everton', 'Felipe', 'Fernando', 'Francisco', 'Franco', 'Fábio', 'Gabriel', 'Gian', 'Guilherme', 'Gustavo', 'Henrique', 'Hernani', 'Horácio', 'Hugo', 'Ian', 'Inácio', 'Isaac', 'Ivan', 'Jerônimo', 'Joaquin', 'Jorge', 'Josué', 'José', 'João', 'Kevin', 'Leandro', 'Leonardo', 'Lucas', 'Luciano', 'Luis', 'Manuel', 'Mateus', 'Matias', 'Miguel', 'Mário', 'Máximo', 'Noel', 'Pablo', 'Paulo', 'Pedro', 'Rafael', 'Ricardo', 'Rodrigo', 'Samuel', 'Santiago', 'Simon', 'Sérgio', 'Thales', 'Thiago', 'Tomás', 'Valentin', 'Vicente', 'Agostinho', 'Demian', 'Giovane', 'Jácomo', 'Martinho', 'Maximiano', 'Natal', 'Sebastião', 'Simão', 'Teobaldo', 'Ziraldo', 'Adriana', 'Agustina', 'Alessandra', 'Alexa', 'Allison', 'Alma', 'Amanda', 'Amélia', 'Ana', 'Andrea', 'Antonieta', 'Ariadna', 'Ariana', 'Ashley', 'Beatriz', 'Bianca', 'Camila', 'Carla', 'Carolina', 'Catarina', 'Clara', 'Daniela', 'Elizabeth', 'Emília', 'Fabiana', 'Fátima', 'Gabriela', 'Giovana', 'Helena', 'Irene', 'Isabel', 'Isabella', 'Isadora', 'Ivana', 'Jasmin', 'Joana', 'Josefina', 'Juliana', 'Julieta', 'Júlia', 'Ketlin', 'Laura', 'Luana', 'Luara', 'Luciana', 'Luna', 'Luzia', 'Madalena', 'Maitê', 'Malena', 'Manuela', 'Mariana', 'Mel', 'Melissa', 'Mia', 'Micaela', 'Michele', 'Miranda', 'Natália', 'Nicole', 'Noelí', 'Norma', 'Nádia', 'Olívia', 'Ornela', 'Paula', 'Paulina', 'Pâmela', 'Rafaela', 'Rebeca', 'Regina', 'Renata', 'Sabrina', 'Salomé', 'Samanta', 'Sara', 'Silvana', 'Sofia', 'Sophie', 'Suzana', 'Taís', 'Tábata', 'Valentina', 'Valéria', 'Violeta', 'Vitória', 'Abgail', 'Constância', 'Hortência', 'Tessália', 'Thalissa'],
+    var nomes = ['Aaron', 'Adriano', 'Alan', 'Alexandre', 'Alonso', 'Anderson', 'Andres', 'Antônio', 'Benjamin', 'Bruno', 'Camilo', 'Carlos', 'Christian', 'Christopher', 'Cristóvão', 'Daniel', 'Dante', 'David', 'Diego', 'Eduardo', 'Elias', 'Emanuel', 'Emiliano', 'Emílio', 'Estêvão', 'Evandro', 'Everton', 'Felipe', 'Fernando', 'Francisco', 'Franco', 'Fábio', 'Gabriel', 'Gian', 'Guilherme', 'Gustavo', 'Henrique', 'Hernani', 'Horácio', 'Hugo', 'Ian', 'Inácio', 'Isaac', 'Ivan', 'Jerônimo', 'Joaquin', 'Jorge', 'Josué', 'José', 'João', 'Kevin', 'Leandro', 'Leonardo', 'Lucas', 'Luciano', 'Luis', 'Manuel', 'Mateus', 'Matias', 'Miguel', 'Mário', 'Máximo', 'Noel', 'Pablo', 'Paulo', 'Pedro', 'Rafael', 'Ricardo', 'Rodrigo', 'Samuel', 'Santiago', 'Simon', 'Sérgio', 'Thales', 'Thiago', 'Tomás', 'Valentin', 'Vicente', 'Agostinho', 'Demian', 'Giovane', 'Jácomo', 'Martinho', 'Maximiano', 'Natal', 'Sebastião', 'Simão', 'Teobaldo', 'Ziraldo'],
         sobrenomes = ['Abreu', 'Azevedo', 'Aguiar', 'Aragão', 'Assunção', 'Aranda', 'Ávila', 'Balestero', 'Barreto', 'Barros', 'Batista', 'Bezerra', 'Beltrão', 'Benites', 'Bittencourt', 'Branco', 'Bonilha', 'Brito', 'Burgos', 'Caldeira', 'Camacho', 'Campos', 'Carmona', 'Carrara', 'Casanova', 'Chaves', 'Cervantes', 'Colaço', 'Cordeiro', 'Corona', 'Correia', 'Cortês', 'Cruz', 'D\'ávila', 'Delatorre', 'Delgado', 'Delvalle', 'Dias', 'Domingues', 'Dominato', 'Duarte', 'Escobar', 'Espinoza', 'Esteves', 'Estrada', 'Faria', 'Faro', 'Feliciano', 'Ferminiano', 'Fernandes', 'Ferraz', 'Ferreira', 'Fidalgo', 'Furtado', 'Ferreira', 'Flores', 'Fonseca', 'Franco', 'Fontes', 'Galindo', 'Galhardo', 'Galvão', 'Garcia', 'Gil', 'Godói', 'Gomes', 'Gonçalves', 'Grego', 'Guerra', 'Gusmão', 'Jimenes', 'Leal', 'Leon', 'Lira', 'Lovato', 'Lozano', 'Lutero', 'Madeira', 'Maldonado', 'Marés', 'Marin', 'Marinho', 'Marques', 'Martines', 'Mascarenhas', 'Matias', 'Matos', 'Maia', 'Medina', 'Meireles', 'Mendes', 'Mendonça', 'Molina', 'Montenegro', 'Neves', 'Oliveira', 'Ortega', 'Ortiz', 'Quintana', 'Queirós', 'Pacheco', 'Padilha', 'Padrão', 'Paes', 'Paz', 'Pedrosa', 'Pena', 'Pereira', 'Perez', 'Prado', 'Pontes', 'Quintana', 'Queirós', 'Ramires', 'Ramos', 'Rangel', 'Rezende', 'Reis', 'Rico', 'Rios', 'Rivera', 'Rocha', 'Rodrigues', 'Romero', 'Roque', 'Rosa', 'Salas', 'Salazar', 'Sales', 'Salgado', 'Sanches', 'Sandoval', 'Santacruz', 'Santana', 'Santiago', 'Saraiva', 'Sepúlveda', 'Serna', 'Serra', 'Serrano', 'Soares', 'Solano', 'Soto', 'Tamoio', 'Teles', 'Toledo', 'Torres', 'Uchoa', 'Urias', 'Valdez', 'Valência', 'Valentin', 'Vale', 'Vasques', 'Vega', 'Velasques', 'Verdugo', 'Verdara', 'Vieira', 'Vila', 'Zamana', 'Zambrano', 'Zaragoça', 'da Cruz', 'da Rosa', 'da Silva', 'das Dores', 'das Neves', 'de Aguiar', 'de Oliveira', 'de Souza'],
+        ddds = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24', '27', '28', '31', '32', '33', '34', '35', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48', '49', '51', '53', '54', '55', '61', '62', '63', '64', '65', '66', '67', '68', '69', '71', '73', '74', '75', '77', '79', '81', '82', '83', '84', '85', '86', '87', '88', '89', '91', '92', '93', '94', '95', '96', '97', '98', '99'],
         companies = ['Dinosaur Coms', 'Crux Networks', 'Sunshine Industries', 'Pixystems', 'Griffindustries', 'Cruisertainment', 'Asco', 'Thunderhive', 'Iceaid', 'Blueking', 'Thor Intelligence', 'Wave Industries', 'Root Arts', 'Amazystems', 'Odinetworks', 'Tuliproductions', 'Odinetworks', 'Heartbeat', 'Marblecast', 'Lionmart', 'Champion Lighting', 'Aura Electronics', 'Grotto Softwares', 'Herolutions', 'Fluxystems', 'Goldustries', 'Whirlwindustries', 'Luckyshadow', 'Starbit', 'Apexaid'];
 
     function randomiza(n) {
@@ -14,6 +15,7 @@ $(function() {
     function gerarCPF() {
         var cpf;
         var comPontos = true;
+        /* TRUE para ativar e FALSE para desativar a pontuação.*/
         var n = 9;
         var n1 = randomiza(n);
         var n2 = randomiza(n);
@@ -38,45 +40,12 @@ $(function() {
         return cpf;
     }
 
-    function removeAcento(str) {
-        {
-            var comAcento = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐ ÑÒÓÔÕÖØÙÚÛÜ ÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ',
-                semAcento = 'AAAAAAACEEEEIIIIDN OOOOOOUUUUYRsBaaa aaaaceeeeiiiionoooooouuuuybyr',
-                novastr = '';
-
-            for (var i = 0; i < str.length; i++) {
-                var troca = false;
-                for (var a = 0; a < comAcento.length; a++) {
-                    if (str.substr(i, 1) === comAcento.substr(a, 1)) {
-                        novastr += semAcento.substr(a, 1);
-                        troca = true;
-                        break;
-                    }
-                }
-                if (troca === false) {
-                    novastr += str.substr(i, 1);
-                }
-            }
-            return novastr;
-        }
-    }
-
-    function gerarCEP() {
-        return randomiza(9).toString() + randomiza(9) + randomiza(9) + randomiza(9) + randomiza(9) + randomiza(9) + randomiza(9) + randomiza(9);
-    }
-
     var fname = nomes[Math.floor(Math.random() * nomes.length)], lname = sobrenomes[Math.floor(Math.random() * sobrenomes.length)];
-    var $registerFormNome = $('#register-form-nome');
-    $registerFormNome.val(fname + ' ' + lname);
+    $('#register-form-nome').val(fname + ' ' + lname);
     $('#register-form-cpf').val(gerarCPF());
-    $('#perfilusuario-cpf').val(gerarCPF());
-    $('#cliente-endcep').val(gerarCEP());
-    $('#register-form-celular').val('52' + 9 + Math.random().toString(10).substring(2, 10));
+    $('#register-form-celular').val(ddds[Math.floor(Math.random() * ddds.length)] + 9 + Math.random().toString(10).substring(2, 10));
     $('#register-form-empresa').val(companies[Math.floor(Math.random() * companies.length)]);
-    if ($registerFormNome.length > 0) {
-        $('#register-form-email').val(removeAcento('samuel' + $registerFormNome.val().replace(' ', '.')) + '@mercadolead.com');
-    }
+    $('#register-form-email').val('samuel+' + fname.toLowerCase() + '.' + lname.toLowerCase() + '@mercadolead.com');
     $('#register-form-password').val(123456);
-    $('#cliente-iccomonosconheceu').val((randomiza(4) + 1).toString());
     $('#register-form-isaceitotermos').prop('checked', true);
 });
